c3c531fa38a38a4d4cf400509a6a4a6e1052d322
asgore-undertale <52295461+asgore-undertale@users.noreply.github.com>
2021-05-02 16:50:55 +0300

    Delete Parts/Parts directory

--- Parts/Parts/Scripts/Sort_lines.py
+++
@@ -1,8 +0,0 @@
-def Sort(text, case = True):
-    lines_list = text.split('\n')
-    lines_list.sort(key=len)
-    
-    if case == False: lines_list = lines_list[::-1]
-    text = '\n'.join(lines_list)
-    
-    return text
