90412b9a6351f8532aee058cd229d4883cca9736
asgore-undertale <52295461+asgore-undertale@users.noreply.github.com>
2021-05-23 09:00:21 +0300

    Delete Scripts directory

--- Scripts/CreateFontTable.py
+++
@@ -1,7 +0,0 @@
-def CreateFontTable(firstX, firstY, BetweenCharsX, BetweenCharsY, unifiedWidth, unifiedHeight, charsPerRow, Chars):
-    table = '\nVERSION="1.0"\nSEPARATOR="█"\n#####################\nChar█X█Y█Width█Height█Xoffset█Yoffset█Xadvance'
-    for c in range(len(Chars)):
-        X = firstX + (c %  charsPerRow * unifiedWidth ) + ((c %  charsPerRow - 1) * BetweenCharsX)
-        Y = firstY + (c // charsPerRow * unifiedHeight) + ((c // charsPerRow - 1) * BetweenCharsY)
-        table += f'\n{Chars[c]}█{X}█{Y}█{unifiedWidth}█{unifiedHeight}█{0}█{0}█{0}'
-    return table
--- Scripts/HandleHarakat.py
+++
@@ -1,6 +0,0 @@
-harakat = 'َﹰﹱﹲﹴﹶﹷﹸﹹﹺﹻﹼﹽﹾﹿ'
-def handle_harakat(text, new_text = ''):
-    text = ' ' + text
-    for _ in range(1, len(text)):
-        if not text[_] in harakat or not text[_ - 1] in harakat: new_text += text[_]
-    return new_text
