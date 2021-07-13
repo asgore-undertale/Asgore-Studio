143ce3799f59aa2c9713ec1d222a506dfa97b444
asgore-undertale <52295461+asgore-undertale@users.noreply.github.com>
2021-07-13 11:13:41 +0300

    Add files via upload

--- Parts/Scripts/ReverseText.py
+++ Parts/Scripts/ReverseText.py
@@ -1,12 +1,12 @@
 import re
 
-bows_list = ['()', '[]', '{}', '<>']
-Arabic_letters = 'ءآأؤإئابةتثجحخدذرزسشصضطظعغفقكلمنهوىي' + '؟،؛ﺀﺁﺂﺃﺄﺅﺆﺇﺈﺉﺊﺋﺌﺍﺎﺏﺐﺑﺒﺓﺔﺕﺖﺗﺘﺙﺚﺛﺜﺝﺞﺟﺠﺡﺢﺣﺤﺥﺦﺧﺨﺩﺪﺫﺬﺭﺮﺯﺰﺱﺲﺳﺴﺵﺶﺷﺸﺹﺺﺻﺼﺽﺾﺿﻀﻁﻂﻃﻄﻅﻆﻇﻈﻉﻊﻋﻌﻍﻎﻏﻐﻑﻒﻓﻔﻕﻖﻗﻘﻙﻚﻛﻜﻝﻞﻟﻠﻡﻢﻣﻤﻥﻦﻧﻨﻩﻪﻫﻬﻭﻮﻯﻰﻱﻲﻳﻴﻵﻶﻷﻸﻹﻺﻻﻼ'
+bowslist = ['()', '[]', '{}', '<>']
+ArabicLetters = 'ءآأؤإئابةتثجحخدذرزسشصضطظعغفقكلمنهوىيپچڤ' + '؟،؛ﺀﺁﺂﺃﺄﺅﺆﺇﺈﺉﺊﺋﺌﺍﺎﺏﺐﺑﺒﺓﺔﺕﺖﺗﺘﺙﺚﺛﺜﺝﺞﺟﺠﺡﺢﺣﺤﺥﺦﺧﺨﺩﺪﺫﺬﺭﺮﺯﺰﺱﺲﺳﺴﺵﺶﺷﺸﺹﺺﺻﺼﺽﺾﺿﻀﻁﻂﻃﻄﻅﻆﻇﻈﻉﻊﻋﻌﻍﻎﻏﻐﻑﻒﻓﻔﻕﻖﻗﻘﻙﻚﻛﻜﻝﻞﻟﻠﻡﻢﻣﻤﻥﻦﻧﻨﻩﻪﻫﻬﻭﻮﻯﻰﻱﻲﻳﻴﻵﻶﻷﻸﻹﻺﻻﻼﭬﭭﭫﭪﭼﭽﭻﭺﭘﭙﭗﭖ'
 
 def swap(text, char1, char2, unused_char = u'\uffff'):
     return text.replace(char1, unused_char).replace(char2, char1).replace(unused_char, char2)
 
-def swap_edges_spaces(text):
+def swapSpacesOnEdges(text):
     if len(text) < 2: return text
     beforeSpaces, afterSpaces = 0, 0
     
@@ -19,20 +19,23 @@ def swap_edges_spaces(text):
     
     return (beforeSpaces * ' ') + text + (afterSpaces * ' ')
 
-def reverse_arabic(text):
-    words = text.split(' ')
-    for w in range(len(words)):
-        if not words[w]: continue
-        if words[w][-1] in Arabic_letters:
-            words[w] = words[w][::-1]
+def reverseArabic(text):
+    container = ''
+    for char in text:
+        if char in ArabicLetters:
+            container += char
+        else:
+            if not container: continue
+            text = text.replace(container, container[::-1], 1)
+            container = ''
     
-    return swap_edges_spaces(' '.join(words[::-1]))
+    return swapSpacesOnEdges(text)
 
 def Reverse(text, case = True):
     if case:
-        for bow in bows_list:
+        for bow in bowslist:
             text = swap(text, bow[0], bow[1])
             text = text[::-1]
-    else: text = reverse_arabic(text)
+    else: text = reverseArabic(text)
     
     return text
